e41187a6034743da8634da47f8e1091cfd5b0151
Suman Dey <sumandey7684@gmail.com>
2024-12-28 11:42:23 +0530

    Create Day 44 - Find All Triplets with Zero Sum.java

---
+++ GfG 160 days DSA Challenge/Hashing/Day 44 - Find All Triplets with Zero Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> findTriplets(int[] arr) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        result.add(Arrays.asList(i, j, k));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
