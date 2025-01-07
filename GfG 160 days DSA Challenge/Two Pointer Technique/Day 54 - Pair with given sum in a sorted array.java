f35d25b5a07fda5d82393415163822a9fa24a894
Suman Dey <sumandey7684@gmail.com>
2025-01-07 09:26:28 +0530

    Create Day 54 - Pair with given sum in a sorted array.java

---
+++ GfG 160 days DSA Challenge/Two Pointer Technique/Day 54 - Pair with given sum in a sorted array.java
@@ -0,0 +1,40 @@
+class Solution {
+    int countPairs(int arr[], int target) {
+        int left = 0, right = arr.length - 1;
+        int count = 0;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                if (arr[left] == arr[right]) {
+                    int numElements = right - left + 1;
+                    count += (numElements * (numElements - 1)) / 2;
+                    break;
+                } else {
+                    int leftCount = 1, rightCount = 1;
+
+                    while (left + 1 < right && arr[left] == arr[left + 1]) {
+                        leftCount++;
+                        left++;
+                    }
+
+                    while (right - 1 > left && arr[right] == arr[right - 1]) {
+                        rightCount++;
+                        right--;
+                    }
+
+                    count += leftCount * rightCount;
+                    left++;
+                    right--;
+                }
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
