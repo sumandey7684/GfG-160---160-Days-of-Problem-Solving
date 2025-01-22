ea6ff2dce7aa69d5292628bce84156791cc26d92
Suman Dey <sumandey7684@gmail.com>
2025-01-22 19:22:55 +0530

    Create Day 68 - Linked List Group Reverse.java

---
+++ GfG 160 days DSA Challenge/Linked List/Day 68 - Linked List Group Reverse.java
@@ -0,0 +1,51 @@
+class Solution {
+    public static Node reverseKGroup(Node head, int k) {
+        int len = getLength(head);
+        Node origH = null, origT = null, tmpH = null, tmpT = null;
+        Node curr = head;
+
+        while (len > 0) {
+            int j = k;
+            j = Math.min(j, len);
+
+            while (j > 0) {
+                Node next = curr.next;
+
+                if (tmpH == null) {
+                    tmpH = curr;
+                    tmpT = curr;
+                } else {
+                    curr.next = tmpH;
+                    tmpH = curr;
+                }
+
+                curr = next;
+                j--;
+                len--;
+            }
+
+            if (origH == null) {
+                origH = tmpH;
+                origT = tmpT;
+            } else {
+                origT.next = tmpH;
+                origT = tmpT;
+            }
+
+            tmpH = null;
+            tmpT = null;
+        }
+
+        origT.next = null;
+        return origH;
+    }
+
+    public static int getLength(Node head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+}
